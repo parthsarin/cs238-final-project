2f1d6e0853b79faa5892d4b16644d2bff6436ded
Jiahui Chen <jchen23@stanford.edu>
2023-12-06 00:45:44 -0800

    reimplement QMDP

--- plan/QMDP.py
+++ plan/QMDP.py
@@ -1,59 +1,48 @@
-import pandas as pd
-from collections import defaultdict
-from tqdm import tqdm
+import numpy as np
 
-def infer_fields(df):
-    c = df.columns
-    return {
-        "o": [c for c in c if c.startswith("o_")],
-        "a": [c for c in c if c.startswith("a_")],
-        "r": [c for c in c if c.startswith("r")],
-        "op": [c for c in c if c.startswith("op_")],
-        "ap": [c for c in c if c.startswith("ap_")],
-    }
+class POMDP:
+    def __init__(self, s, a, o, t, z, r, gamma):
+        self.s = s  # states
+        self.a = a  # actions
+        self.o = o  # observations
+        self.t = t  # transition function
+        self.z = z  # observation function
+        self.r = r  # reward function
+        self.gamma = gamma  # discount factor
 
-def QMDP(df, k_max=50, gamma=0.95):
-    fields = infer_fields(df)
-    Q = defaultdict(float)  # Initialize Q-values
+class AlphaVectorPolicy:
+    def __init__(self, p, r, a):
+        self.p = p  # POMDP problem
+        self.r = r  # alpha vectors
+        self.a = a  # actions associated with alpha vectors
 
-    # Simplified state and action extraction
-    states = df[fields['o']].drop_duplicates().to_numpy()
-    actions = df[fields['a']].drop_duplicates().to_numpy()
+def utility(pi, b):
+    return max(np.dot(alpha, b) for alpha in pi.r)
 
-    # Simplified transition probability estimation
-    def estimate_transition_probabilities(df, current_state, next_state):
-        # Very basic estimation: frequency of direct transition
-        total_transitions = len(df)
-        specific_transitions = len(df[(df[fields['o']] == current_state) & (df[fields['op']] == next_state)])
-        return specific_transitions / total_transitions if total_transitions > 0 else 0
+def choose_action(pi, b):
+    i = np.argmax([np.dot(alpha, b) for alpha in pi.r])
+    return pi.a[i]
 
-    def update(Q, df, gamma):
-        for state in states:
-            state_tuple = tuple(state)  # Convert state array to tuple
-            for action in actions:
-                action_tuple = tuple(action)  # Convert action array to tuple
-                # Calculate reward for this state-action pair
-                reward = df[(df[fields['o']] == state_tuple) & (df[fields['a']] == action_tuple)]['r'].mean()
+class QMDP:
+    def __init__(self, k_max):
+        self.k_max = k_max  # maximum number of iterations
 
-                # Calculate expected utility
-                expected_utility = 0
-                for next_state in states:
-                    next_state_tuple = tuple(next_state)  # Convert next state array to tuple
-                    transition_probability = estimate_transition_probabilities(df, state_tuple, next_state_tuple)
-                    max_q_value = max(Q[next_state_tuple, a_tuple] for a_tuple in (tuple(a) for a in actions))
-                    expected_utility += transition_probability * max_q_value
+def solve(m: QMDP, p: POMDP):
+    r = [zeros(len(p.s)) for a in p.a]
+    r = alphavector_iteration(m, p, r)
+    return AlphaVectorPolicy(p, r, p.a)
 
-                # Update Q-value
-                Q[state_tuple, action_tuple] = reward + gamma * expected_utility
+def update(p: POMDP, m: QMDP, r):
+    s, a, r, t, gamma = p.s, p.a, p.r, p.t, p.gamma
+    r_prime = [[r[s, a] + gamma * sum(t(s, a, s_prime) * max(r_prime[j] for r_prime in r)
+               for j, s_prime in enumerate(s)) for s in s] for a in a]
+    return r_prime
 
-        return Q
+def alphavector_iteration(p, m, r):
+    for k in range(1, m.k_max + 1):
+        r = update(p, m, r)
+    return r
 
-    # Run alpha vector iteration
-    for _ in tqdm(range(k_max)):
-        Q = update(Q, df, gamma)
+def zeros(length):
+    return [0] * length
 
-    return Q
-
-# Usage of the function
-# df = pd.read_csv('your_dataframe.csv')  # Load your DataFrame here
-# Q_values = QMDP(df, k_max=100, gamma=0.95)
