17f1f9155cd06619c24ef292b044ec101b969ae5
Parth Sarin <psarin@stanford.edu>
2023-12-06 23:27:22 -0800

    dqn memoryless

--- learn.py
+++ learn.py
@@ -4,39 +4,70 @@ File: learn.py
 
 This file implements the QLearning algorithm on a pandas dataframe.
 """
-import pandas as pd
-from plan.QLearning import sarsa, infer_fields
-from policy.FromQ import QPolicy
-from env import StudentAction, TeacherAction
-from pickle import dump
+# from policy.DeepQMemoryless.Student import StudentQ, StudentPolicy
+from policy.RandomPolicy import StudentPolicy
+from policy.DeepQMemoryless.Teacher import TeacherQ, TeacherPolicy
 
-DATA_NAME = "50y-1m"
+from env import Classroom
+
+import torch
+
+# sQ = StudentQ(32)
+# sQ.load_state_dict(torch.load("model/deep-q-memoryless-student.pt"))
+
+tQ = TeacherQ(32)
+opt = torch.optim.Adam(tQ.parameters(), lr=0.001)
 
 
 def main():
-    print("1. student")
-    df = pd.read_csv(f"data/{DATA_NAME}/student-random.csv")
-    fields = infer_fields(df)
+    # run a simulation of the first 14 days with 35 students and then restart,
+    # keep track of the loss
+    sπ = StudentPolicy()
+    tπ = TeacherPolicy(tQ)
+    c = Classroom(35)
+    loss = 0
 
-    Q = sarsa(df, fields=fields, verbose=True)
-    obs_ordering = [f[2:] for f in fields['o']]
-    act_ordering = [f[2:] for f in fields['a']]
-    dump(
-        {"Q": Q, "obs_ordering": obs_ordering, "act_ordering": act_ordering},
-        open(f"model/{DATA_NAME}-student-Q.pkl", "wb")
-    )
+    for epoch in range(100_000):
+        # reset the classroom
+        c = Classroom(35)
 
-    print("2. teacher")
-    df = pd.read_csv(f"data/{DATA_NAME}/teacher-random.csv")
-    fields = infer_fields(df)
+        if epoch < 500:
+            horizon = 5
+        elif epoch < 1000:
+            horizon = 14
+        else:
+            horizon = 100
 
-    Q = sarsa(df, fields=fields, verbose=True)
-    obs_ordering = [f[2:] for f in fields['o']]
-    act_ordering = [f[2:] for f in fields['a']]
-    dump(
-        {"Q": Q, "obs_ordering": obs_ordering, "act_ordering": act_ordering},
-        open(f"model/{DATA_NAME}-teacher-Q.pkl", "wb")
-    )
+        # run the simulation
+        for t in range(horizon):
+            # student actions
+            student_os = [h[-1][0] for h in c.student_h]
+            student_as = [sπ[h] for h in c.student_h]
+            student_rs = c.student_step(student_as, t)
+
+            # teacher action
+            teacher_o = c.teacher_h[-1][0]
+            teacher_a = tπ[c.teacher_h]
+            teacher_r = c.teacher_step(teacher_a, t)
+            teacher_op = c.teacher_h[-1][0]
+
+            # update the policy
+            loss += tπ.loss(teacher_o, teacher_a, teacher_r, teacher_op)
+
+        if epoch % 5 == 0:
+            # update the policy
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            # print the loss
+            print(f"[epoch {epoch}] loss = {loss.item()}")
+
+            # reset the loss
+            loss = 0
+
+            # write the model
+            torch.save(tQ.state_dict(), "model/deep-q-memoryless-teacher.pt")
 
 
 if __name__ == "__main__":
--- visualize.py
+++ visualize.py
@@ -1,6 +1,7 @@
 from simulate import simulate
 from evaluate.plot import plot_rs
 from env import StudentAction, TeacherAction
+import torch
 
 
 def main():
@@ -13,15 +14,27 @@ def main():
     sπ = load_qpolicy("model/50y-student-Q.pkl", StudentAction)
     tπ = load_qpolicy("model/50y-teacher-Q.pkl", TeacherAction)
     l_q = simulate(35, 365, sπ, tπ)
+    # print(f"student took {sπ.num_rand} random actions, {sπ.num_q} q actions")
+    # print(f"teacher took {tπ.num_rand} random actions, {tπ.num_q} q actions")
 
-    print(f"student took {sπ.num_rand} random actions, {sπ.num_q} q actions")
-    print(f"teacher took {tπ.num_rand} random actions, {tπ.num_q} q actions")
+    from policy.DeepQMemoryless.Student import StudentQ, StudentPolicy
+    sQ = StudentQ(32)
+    sQ.load_state_dict(torch.load("model/deep-q-memoryless-student.pt"))
+    sπ = StudentPolicy(sQ)
+
+    from policy.DeepQMemoryless.Teacher import TeacherQ, TeacherPolicy
+    tQ = TeacherQ(32)
+    tQ.load_state_dict(torch.load("model/deep-q-memoryless-teacher.pt"))
+    tπ = TeacherPolicy(tQ)
+
+    l_q_deeplearned = simulate(35, 365, sπ, tπ)
 
     plot_rs(
-        [l_random, l_q],
+        [l_random, l_q, l_q_deeplearned],
         [
             ("random policy", "random policy"),
-            ("q-learned", "q-learned")
+            ("q-learned", "q-learned"),
+            ("deep q-learned", "deep q-learned")
         ]
     )
 
