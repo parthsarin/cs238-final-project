90aebe20273bae6c971db67ab062655134bc4905
Jiahui Chen <jchen23@stanford.edu>
2023-12-07 23:39:25 -0800

    reverted learn.py

--- learn.py
+++ learn.py
@@ -2,54 +2,42 @@
 File: learn.py
 --------------
 
-This file implements learning algorithms on a pandas dataframe.
+This file implements the QLearning algorithm on a pandas dataframe.
 """
 import pandas as pd
-import argparse
 from plan.QLearning import sarsa, infer_fields
-from plan.QMDP import QMDP, infer_fields
 from policy.FromQ import QPolicy
 from env import StudentAction, TeacherAction
 from pickle import dump
 
 DATA_NAME = "50y-1m"
 
-def main(algorithm):
+
+def main():
     print("1. student")
     df = pd.read_csv(f"data/{DATA_NAME}/student-random.csv")
     fields = infer_fields(df)
 
-    if algorithm == 'sarsa':
-        Q = sarsa(df, fields=fields, verbose=True)
-    elif algorithm == 'qmdp':
-        Q = QMDP(df)
-
+    Q = sarsa(df, fields=fields, verbose=True)
     obs_ordering = [f[2:] for f in fields['o']]
     act_ordering = [f[2:] for f in fields['a']]
     dump(
         {"Q": Q, "obs_ordering": obs_ordering, "act_ordering": act_ordering},
-        open(f"model/{DATA_NAME}-student-{algorithm.upper()}.pkl", "wb")
+        open(f"model/{DATA_NAME}-student-Q.pkl", "wb")
     )
 
     print("2. teacher")
     df = pd.read_csv(f"data/{DATA_NAME}/teacher-random.csv")
     fields = infer_fields(df)
 
-    if algorithm == 'sarsa':
-        Q = sarsa(df, fields=fields, verbose=True)
-    elif algorithm == 'qmdp':
-        Q = QMDP(df)
-
+    Q = sarsa(df, fields=fields, verbose=True)
     obs_ordering = [f[2:] for f in fields['o']]
     act_ordering = [f[2:] for f in fields['a']]
     dump(
         {"Q": Q, "obs_ordering": obs_ordering, "act_ordering": act_ordering},
-        open(f"model/{DATA_NAME}-teacher-{algorithm.upper()}.pkl", "wb")
+        open(f"model/{DATA_NAME}-teacher-Q.pkl", "wb")
     )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run a learning algorithm on a DataFrame.")
-    parser.add_argument("algorithm", choices=['sarsa', 'qmdp'], help="The learning algorithm to use (sarsa or qmdp)")
-    args = parser.parse_args()
-    main(args.algorithm)
+    main()
