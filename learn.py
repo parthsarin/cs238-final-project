88d8435859511436fc2b74b97e3f871bcbf0691a
Parth Sarin <psarin@stanford.edu>
2023-12-09 23:32:37 -0800

    self-play dqn

--- learn.py
+++ learn.py
@@ -4,69 +4,98 @@ File: learn.py
 
 This file implements the QLearning algorithm on a pandas dataframe.
 """
-from policy.RandomPolicy import StudentPolicy, TeacherPolicy
-# from policy.DeepQ.Teacher import TeacherQ, TeacherPolicy
+# from policy.RandomPolicy import StudentPolicy, TeacherPolicy
+# from policy.DeepQMemoryless.Student import StudentQ, StudentPolicy
+# from policy.DeepQMemoryless.Teacher import TeacherQ, TeacherPolicy
+from policy.DeepQ.Teacher import TeacherQ, TeacherPolicy
 from policy.DeepQ.Student import StudentQ, StudentPolicy
 
 from env import Classroom
 
 import torch
 
-sQ = StudentQ(16, 32)
-opt = torch.optim.Adam(sQ.parameters(), lr=0.001)
-# sQ.load_state_dict(torch.load("model/deep-q-memoryless-student.pt"))
+sQ = StudentQ(3, 32)
+s_opt = torch.optim.Adam(sQ.parameters(), lr=0.001)
+# sQ.load_state_dict(torch.load("model/deep-q-student.pt"))
 
-# tQ = TeacherQ(16, 128)
-# opt = torch.optim.Adam(tQ.parameters(), lr=0.001)
+tQ = TeacherQ(3, 128)
+t_opt = torch.optim.Adam(tQ.parameters(), lr=0.001)
+# tQ.load_state_dict(torch.load("model/deep-q-teacher.pt"))
 
 
 def main():
     # run a simulation of the first 14 days with 35 students and then restart,
     # keep track of the loss
     sπ = StudentPolicy(sQ)
-    tπ = TeacherPolicy()
+    tπ = TeacherPolicy(tQ)
     c = Classroom(35)
-    loss = 0
+    s_loss = t_loss = 0
 
     for epoch in range(100_000):
         # reset the classroom
         c = Classroom(35)
 
+        if epoch < 100:
+            horizon = 5
+        elif epoch < 500:
+            # ramp up to 21
+            horizon = 5 + (epoch - 100) // 21
+            sπ.eps = tπ.eps = 0.3
+        else:
+            horizon = 21
+            sπ.eps = tπ.eps = 0.5
+
         # run the simulation
-        for t in range(5):
+        for t in range(horizon):
             # student actions
             student_as = []
             student_qs = []
             for h in c.student_h:
                 a, q = sπ[h]
+                # a = sπ[h]
                 student_as.append(a)
                 student_qs.append(q)
             student_rs = c.student_step(student_as, t)
 
             # teacher action
-            # teacher_a, q_vals = tπ[c.teacher_h]
-            teacher_a = tπ[c.teacher_h]
+            teacher_a, q_vals = tπ[c.teacher_h]
+            # teacher_a = tπ[c.teacher_h]
             teacher_r = c.teacher_step(teacher_a, t)
 
             # update the policy
             student_hs = [h for h in c.student_h]
             for (q, a, r, h) in zip(student_qs, student_as, student_rs, student_hs):
-                loss += sπ.loss(q, a, r, h)
-            # loss += tπ.loss(q_vals, teacher_a, teacher_r, c.teacher_h)
+                # for (a, r, h) in zip(student_as, student_rs, student_hs):
+                #     o = h[-2][0]
+                #     op = h[-1][0]
+                s_loss += sπ.loss(q, a, r, h)
+                # s_loss += sπ.loss(o, a, r, op)
+            t_loss += tπ.loss(q_vals, teacher_a, teacher_r, c.teacher_h)
+            # o = c.teacher_h[-2][0]
+            # op = c.teacher_h[-1][0]
+            # t_loss += tπ.loss(o, teacher_a, teacher_r, op)
 
-        # update the policy
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
+        # update the student policy
+        s_opt.zero_grad()
+        s_loss.backward()
+        s_opt.step()
+
+        # update the teacher policy
+        t_opt.zero_grad()
+        t_loss.backward()
+        t_opt.step()
 
         # print the loss
-        print(f"[epoch {epoch}] loss = {loss.item()}")
+        print(
+            f"[epoch {epoch}] student loss = {round(s_loss.item(), 2)}, teacher loss = {round(t_loss.item(), 2)}"
+        )
 
         # reset the loss
-        loss = 0
+        s_loss = t_loss = 0
 
         # write the model
-        torch.save(sQ.state_dict(), "model/deep-q-student.pt")
+        torch.save(sQ.state_dict(), "model/deep-q-student-2.pt")
+        torch.save(tQ.state_dict(), "model/deep-q-teacher-2.pt")
 
 
 if __name__ == "__main__":
--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ from simulate import simulate
 from evaluate.plot import plot_rs
 from env import StudentAction, TeacherAction
 import torch
+import pandas as pd
 
 
 def main():
@@ -11,23 +12,29 @@ def main():
     tπ = TeacherPolicy()
     l_random = simulate(35, 365, sπ, tπ)
 
-    print("running q table simulation")
-    from policy.FromQ import load_qpolicy
-    sπ = load_qpolicy("model/50y-student-Q.pkl", StudentAction)
-    tπ = load_qpolicy("model/50y-teacher-Q.pkl", TeacherAction)
-    l_q = simulate(35, 365, sπ, tπ)
+    # print("running q table simulation")
+    # from policy.FromQ import load_qpolicy
+    # sπ = load_qpolicy("model/50y-student-Q.pkl", StudentAction)
+    # tπ = load_qpolicy("model/50y-teacher-Q.pkl", TeacherAction)
+    # l_q = simulate(35, 365, sπ, tπ)
 
-    print("running epsilon-greedy simulation")
-    from policy.FromEpsilonGreedy import load_epsilon_greedy_policy
-    sπ = load_epsilon_greedy_policy("model/50y-student-Q.pkl", StudentAction)
-    tπ = load_epsilon_greedy_policy("model/50y-teacher-Q.pkl", TeacherAction)
-    l_e = simulate(35, 365, sπ, tπ)
+    # print("running q + li simulation")
+    # from policy.FromQ import load_qpolicy
+    # sπ = load_qpolicy("model/50y-1m-student-Q-li.pkl", StudentAction)
+    # tπ = load_qpolicy("model/50y-1m-teacher-Q-li.pkl", TeacherAction)
+    # l_q_li = simulate(35, 365, sπ, tπ)
 
-    print("running ucb1 simulation")
-    from policy.FromUCB1 import load_ucb1_policy
-    sπ = load_ucb1_policy("model/50y-student-Q.pkl", StudentAction)
-    tπ = load_ucb1_policy("model/50y-teacher-Q.pkl", TeacherAction)
-    l_ucb1 = simulate(35, 365, sπ, tπ)
+    # print("running epsilon-greedy simulation")
+    # from policy.FromEpsilonGreedy import load_epsilon_greedy_policy
+    # sπ = load_epsilon_greedy_policy("model/50y-student-Q.pkl", StudentAction)
+    # tπ = load_epsilon_greedy_policy("model/50y-teacher-Q.pkl", TeacherAction)
+    # l_e = simulate(35, 365, sπ, tπ)
+
+    # print("running ucb1 simulation")
+    # from policy.FromUCB1 import load_ucb1_policy
+    # sπ = load_ucb1_policy("model/50y-student-Q.pkl", StudentAction)
+    # tπ = load_ucb1_policy("model/50y-teacher-Q.pkl", TeacherAction)
+    # l_ucb1 = simulate(35, 365, sπ, tπ)
 
     print("running deep q, memoryless simulation")
     from policy.DeepQMemoryless.Student import StudentQ, StudentPolicy
@@ -44,24 +51,33 @@ def main():
 
     print("running deep q simulation")
     from policy.DeepQ.Teacher import TeacherQ, TeacherPolicy
-    tQ = TeacherQ(16, 128)
-    tQ.load_state_dict(torch.load("model/deep-q-teacher.pt"))
+    tQ = TeacherQ(3, 128)
+    tQ.load_state_dict(torch.load("model/deep-q-teacher-memoryless-2.pt"))
     tπ = TeacherPolicy(tQ, train=False)
 
     from policy.DeepQ.Student import StudentQ, StudentPolicy
-    sQ = StudentQ(16, 32)
-    sQ.load_state_dict(torch.load("model/deep-q-student.pt"))
+    sQ = StudentQ(3, 32)
+    sQ.load_state_dict(torch.load("model/deep-q-student-memoryless-2.pt"))
     sπ = StudentPolicy(sQ, train=False)
 
     l_qdl = simulate(35, 365, sπ, tπ)
 
     plot_rs(
-        [l_random, l_q, l_e, l_ucb1, l_qdl_memoryless, l_qdl],
+        [
+            l_random,
+            # l_q,
+            # l_q_li,
+            # l_e,
+            # l_ucb1,
+            l_qdl_memoryless,
+            l_qdl
+        ],
         [
             ("random policy", "random policy"),
-            ("q-learned", "q-learned"),
-            ("epsilon-greedy", "epsilon-greedy"),
-            ("ucb1", "ucb1"),
+            # ("q-learning", "q-learning"),
+            # ("q + linear interpolation", "q + linear interpolation"),
+            # ("q + ε-greedy", "q + ε-greedy"),
+            # ("q + ucb1", "q + ucb1"),
             ("dqn (memoryless)", "dqn (memoryless)"),
             ("dqn", "dqn")
         ]
